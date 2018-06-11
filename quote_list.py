f5dd0897a7b418113fe3c10dd376787996de6bf5
Ryan O'Mullan <omullan.ryan@gmail.com>
2018-06-11 10:12:30 -0300

    Create quote_list.py

---
+++ quote_list.py
@@ -0,0 +1,214 @@
+quote_list = [
+    {
+        'quote': "I think it is possible for ordinary people to choose to be extraordinary.",
+        "author": 'Elon Musk'
+    },
+    {
+        'quote': "Your time is limited, so don't waste it living someone else's life.",
+        "author": 'Steve Jobs'
+    },
+    {
+        'quote': "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
+        "author": 'Mark Twain'
+    },
+    {
+        'quote': "When you're different, sometimes you don't see the millions of people who accept you for what you are. All you notice is the person who doesn't.",
+        "author": 'Jodi Picoult'
+    },
+    {
+        'quote': "The best time to plant a tree was 20 years ago. The second best time is now.",
+        "author": 'Chinese proverb'
+    },
+    {
+        'quote': "Keep on going, and the chances are that you will stumble on something, perhaps when you are least expecting it. I never heard of anyone ever stumbling on something sitting down.",
+        "author": 'Charles F. Kettering'
+    },
+    {
+        'quote': "Whenever you see a successful person, you only see the public glories, never the private sacrifices to reach them.",
+        "author": 'Vaibhav Shah'
+    },
+    {
+        'quote': "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+        "author": 'Vince Lombardi'
+    },
+    {
+        'quote': "The real test is not whether you avoid failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
+        "author": 'Barack Obama'
+    },
+    {
+        'quote': "Life is 10% what happens to me and 90% of how I react to it.",
+        "author": 'Charles Swindoll'
+    },
+    {
+        'quote': "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        "author": 'Oprah'
+    },
+    {
+        'quote': "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+        "author": 'Jimmy Dean'
+    },
+    {
+        'quote': "The art of living lies less in eliminating our troubles than growing with them.",
+        "author": 'Bernard M. Baruch'
+    },
+    {
+        'quote': "I am not a product of my circumstances. I am a product of my decisions.",
+        "author": 'Stephen Covey'
+    },
+    {
+        'quote': "I find that the harder I work, the more luck I seem to have.",
+        "author": 'Thomas Jefferson'
+    },
+    {
+        'quote': "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
+        "author": 'Henry Ford'
+    },
+    {
+        'quote': "The most common way people give up their power is by thinking they don't have any.",
+        "author": 'Alice Walker'
+    },
+    {
+        'quote': "Whenever you see a successful business, someone once made a courageous decision.",
+        "author": 'Peter Drucker'
+    },
+    {
+        'quote': "Success is the sum of small efforts, repeated day in and day out.",
+        "author": 'Robert Collier'
+    },
+    {
+        'quote': "Would you like me to give you a formula for success? It's quite simple, really: Double your rate of failure. You are thinking of failure as the enemy of success. But it isn't at all. You can be discouraged by failure or you can learn from it, so go ahead and make mistakes. Make all you can. Because remember that's where you will find success.",
+        "author": 'Thomas Watson'
+    },
+    {
+        'quote': "The most difficult thing is the decision to act. The rest is merely tenacity.",
+        "author": 'Amelia Earhart'
+    },
+    {
+        'quote': "Successful and unsuccessful people do not vary greatly in their abilities. They vary in their desires to reach their potential.",
+        "author": 'John Maxwell'
+    },
+    {
+        'quote': "The only place where success comes before work is in the dictionary.",
+        "author": 'Vidal Sassoon'
+    },
+    {
+        'quote': "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        "author": 'Robert Louis Stevenson'
+    },
+    {
+        'quote': "The question isn't who is going to let me; it's who is going to stop me.",
+        "author": 'Ayn Rand'
+    },
+    {
+        'quote': "Build your own dreams, or someone else will hire you to build theirs.",
+        "author": 'Farrah Gray'
+    },
+    {
+        'quote': "Though no one can go back and make a brand-new start, anyone can start from now and make a brand new ending.",
+        "author": 'Carl Bard'
+    },
+    {
+        'quote': "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
+        "author": 'Dalai Lama'
+    },
+    {
+        'quote': "I would rather die of passion than of boredom.",
+        "author": 'Vincent van Gogh'
+    },
+    {
+        'quote': "A person who never made a mistake never tried anything new.",
+        "author": 'Albert Einstein'
+    },
+    {
+        'quote': "The successful warrior is the average man with laser-like focus.",
+        "author": 'Bruce Lee'
+    },
+    {
+        'quote': "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.",
+        "author": 'Leonardo da Vinci'
+    },
+    {
+        'quote': "If you want to lift yourself up, lift up someone else.",
+        "author": 'Booker T. Washington'
+    },
+    {
+        'quote': "Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.",
+        "author": 'Jamie Paolinetti'
+    },
+    {
+        'quote': """When I was 5 years old, my mother always told me that happiness was the key to life. When I went to school, they asked me what I wanted to be when I grew up. I wrote down "happy". They told me I didn't understand the assignment, and I told them they didn't understand life.""",
+        "author": 'John Lennon'
+    },
+    {
+        'quote': "The only person you are destined to become is the person you decide to be.",
+        "author": 'Ralph Waldo Emerson'
+    },
+    {
+        'quote': "If you genuinely want something, don't wait for it--teach yourself to be impatient.",
+        "author": 'Gurbaksh Chahal'
+    },
+    {
+        'quote': "Everything you've ever wanted is on the other side of fear.",
+        "author": 'George Addair'
+    },
+    {
+        'quote': "Nothing will work unless you do.",
+        "author": 'Maya Angelou'
+    },
+    {
+        'quote': "If you really look closely, most overnight successes took a long time.",
+        "author": 'Steve Jobs'
+    },
+    {
+        'quote': "The No. 1 reason people fail in life is because they listen to their friends, family, and neighbors.",
+        "author": 'Napoleon Hill'
+    },
+    {
+        'quote': "I never dreamed about success, I worked for it.",
+        "author": 'Estee Lauder'
+    },
+    {
+        'quote': "To be successful, you must accept all challenges that come your way. You can't just accept the ones you like.",
+        "author": 'Mike Gafka'
+    },
+    {
+        'quote': "Don't wait until everything is just right. It will never be perfect. There will always be challenges, obstacles, and less than perfect conditions. So what? Get started now. With each step you take, you will grow stronger and stronger, more and more skilled, more and more self-confident, and more and more successful.",
+        "author": 'Mark Victor Hansen'
+    },
+    {
+        'quote': "You wouldn't worry so much about what others think of you if you realized how seldom they do.",
+        "author": 'Eleanor Roosevelt'
+    },
+    {
+        'quote': "Low self-confidence isn't a life sentence. Self-confidence can be learned, practiced, and mastered -- just like any other skill. Once you master it, everything in your life will change for the better.",
+        "author": 'Barrie Davenport'
+    },
+    {
+        'quote': "Trust yourself. Create the kind of self that you will be happy to live with all your life. Make the most of yourself by fanning the tiny, inner sparks of possibility into flames of achievement.",
+        "author": 'Golda Meir'
+    },
+    {
+        'quote': "Successful people have fear, successful people have doubts, and successful people have worries. They just don't let these feelings stop them.",
+        "author": 'T. Harv Eker'
+    },
+    {
+        'quote': "You can have anything you want if you are willing to give up the belief that you can't have it.",
+        "author": 'Dr. Robert Anthony'
+    },
+    {
+        'quote': "It is not the mountain we conquer, but ourselves.",
+        "author": 'Sir Edmund Hillary'
+    },
+    {
+        'quote': "Inaction breeds doubt and fear. Action breeds confidence and courage. If you want to conquer fear, do not sit home and think about it. Go out and get busy.",
+        "author": 'Dale Carnegie'
+    },
+    {
+        'quote': "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude.",
+        "author": 'Thomas Jefferson'
+    },
+    {
+        'quote': "The best revenge is massive success.",
+        "author": 'Frank Sinatra'
+    }
+]
